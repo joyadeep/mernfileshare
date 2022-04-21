2cef42237a5fe8f0628705bf66b50a2e95bb1434
joyadeep <joyadeep.limbu@gmail.com>
2022-04-21 23:03:02 +0545

    worked on file upload

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+interface Ifiletype{
+  name:string,
+  size:number,
+  type:string
+}
 
 const Home = () => {
+  const [file,setFile]=useState<File>();
+  const handleChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
+
+    const filelist=e.target.files;
+
+    if(!filelist) return;
+    
+    setFile(filelist[0])
+  };
+  
+   const uploadFile =(e: React.MouseEvent<HTMLSpanElement, MouseEvent>)=> {
+     if (file) {
+         const formData = new FormData();
+         formData.append("myFile", file, file.name);
+     }
+
+  console.log(uploadFile);
+  
+    
+    
+    
+  }
   return (
-    <div className='w-full h-fit bg-blue-100 rounded-md w-1/3 pt-3 pb-5 px-5 shadow-lg '>
-        <form className='text-center space-y-5 flex flex-col'>
+    <div className='h-fit bg-blue-100 border-2 border-slate-300 rounded-md w-1/3 pt-3 pb-5 px-5 shadow-lg '>
+        <form className='text-center space-y-5 mb-5 flex flex-col'>
             <h1 className='text-3xl font-bold text-slate-600'>Upload file to share</h1>
-            <input type="file" />
-            <button className='bg-green-500 px-2 py-1 rounded-sm text-white text-xl font-semibold'>Upload</button>
+            <input type="file" accept='imgge/*' onChange={handleChange} />
+            <button onClick={uploadFile} className='bg-green-500 px-2 py-1 rounded-sm text-white text-xl font-semibold'>Upload</button>
         </form>
-        <h1>Upload your file and share with your friends without any hassle. Just give it a try !! </h1>
+        <h1 className='text-justify'>Upload your file and share with your friends without any hassle. Just give it a try while keeping yourself anonymous </h1>
     </div>
   )
 }
